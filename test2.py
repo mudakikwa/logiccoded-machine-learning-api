8ed3b2b12d40ffd699800f5a11e3bc939170e685
mudakikwa <mudakikwastory@gmail.com>
2021-05-26 20:41:08 +0200

    finishhed movie dataset coding

--- test2.py
+++ test2.py
@@ -19,5 +19,16 @@ cosine_sim=cosine_similarity(counter_matrix)
 def get_title_from_index(index):
     return df[df.index == index]["title"].values[0]
 def get_index_from_title(title):
-    return df[df.title == title]["index"].value[0]
-    
+    return df[df.title == title]["index"].values[0]
+
+movie_user_likes="Avatar"   
+movie_index=get_index_from_title(movie_user_likes)
+similar_movies=list(enumerate(cosine_sim[movie_index]))
+sorted_similar_movies=sorted(similar_movies,key=lambda x:x[1],reverse=True)[1:]
+i=0
+print("Top 5 similar movies to "+ movie_user_likes +"are: \n")
+for element in sorted_similar_movies:
+    print(get_title_from_index(element[0]))
+    i = i+1
+    if i > 5:
+        break
