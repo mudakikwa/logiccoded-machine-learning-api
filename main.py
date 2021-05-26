f15cb360d1fb958ccb498b317b0f0b0153245491
mudakikwa <mudakikwastory@gmail.com>
2021-05-26 19:17:01 +0200

    finished the setup for flask

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 # started the ml model
-from flask import Flask, request,jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/query", methods=["POST", "GET"])
 def getCode():
     if(request.method == 'POST'):
-        return jsonify(request.form("query"))
-    return "ready"
+        return jsonify({"query": request.form["query"]})
 
 
 if __name__ == "__main__":
