106fc936048b891ec51951cf42c6c124e1c15c6c
mudakikwa <mudakikwastory@gmail.com>
2021-05-26 19:43:08 +0200

    started the machine language test

---
+++ test.py
@@ -0,0 +1,10 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+text = ["London Paris London", "Paris Paris London"]
+cv=CountVectorizer()
+count_matrix=cv.fit_transform(text)
+print(cv.get_feature_names())
+print(count_matrix.toarray())
+similarity_score=cosine_similarity(count_matrix)
+print(similarity_score)
+
