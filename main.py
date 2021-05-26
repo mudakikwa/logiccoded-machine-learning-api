49fd21dac0e89b5c9aa46842633cf11c924eae22
mudakikwa <mudakikwastory@gmail.com>
2021-05-26 18:51:01 +0200

    boostraped the flask project

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#started the ml model
+# started the ml model
+from flask import Flask, request,jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST", "GET"])
+def getCode():
+    if(request.method == 'POST'):
+        return jsonify(request.form("query"))
+    return "ready"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
