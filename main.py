08125227bac5af620587004c70412c78c401580e
mudakikwa <mudakikwastory@gmail.com>
2021-06-01 00:10:10 +0200

    added the apu intergration

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 # started the ml model
 from flask import Flask, request, jsonify
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from termcolor import colored
 
 app = Flask(__name__)
 
@@ -7,5 +12,35 @@ app = Flask(__name__)
 @app.route("/query", methods=["POST", "GET"])
 def getCode():
     if(request.method == 'POST'):
-        return jsonify({"query": request.form["query"]})
+        df = pd.read_csv("dataset.csv")
+        feature = ['title', 'Code Description', 'Functionality Language']
+
+        def extract_feuture(row):
+            return row["title"]+" " + row["Code Description"]+" "+row["Functionality Language"]
+
+        for feature in feature:
+            df[feature] = df[feature].fillna("")
+
+        df["combined_features"] = df.apply(extract_feuture, axis=1)
+        cv = CountVectorizer()
+        count_matrix = cv.fit_transform(df["combined_features"])
+        cosine_sim = cosine_similarity(count_matrix)
+
+        def get_code_from_index(index):
+            try:
+                return print(df.loc[df['number'] == index]["code"].values[0])
+            except Exception:
+                pass
+
+        def get_index_from_title(title):
+            index = df.index
+            return index[df["title"] == title].values[0]+1
+
+        code_title = request.form["query"]
+        code_index = get_index_from_title(code_title)
+        similar_code = list(enumerate(cosine_sim[code_index]))
+        sorted_code = sorted(similar_code, key=lambda x: x[1], reverse=True)
+        i = 0
+        return jsonify({"query": get_code_from_index(element[0])})
+
     return "<h1>Welcome </h1>"
